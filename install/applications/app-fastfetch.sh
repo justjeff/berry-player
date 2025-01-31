7bba3b0b2338cef9a445658f1f68eba5c38a92d5
justjeff <211851+justjeff@users.noreply.github.com>
2025-01-31 17:44:47 -0600

    Alt install

--- install/applications/app-fastfetch.sh
+++ install/applications/app-fastfetch.sh
@@ -1,7 +1,12 @@
 # Display system information in the terminal
-sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
-sudo apt update -y
-sudo apt install -y fastfetch
+# sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
+# sudo apt update -y
+# sudo apt install -y fastfetch
+
+wget -qO fastfetch.tar.gz https://github.com/fastfetch-cli/fastfetch/releases/latest/download/fastfetch-linux-aarch64.tar.gz
+sudo tar xf fastfetch.tar.gz --strip-components=3 -C /usr/local/bin fastfetch-linux-aarch64/usr/bin/fastfetch
+
+
 
 # Only attempt to set configuration if fastfetch is not already set
 if [ ! -f "$HOME/.config/fastfetch/config.jsonc" ]; then
