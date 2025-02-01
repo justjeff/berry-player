47cb77cc00a71474602c94a95613602463e7ae3f
justjeff <211851+justjeff@users.noreply.github.com>
2025-01-31 23:41:35 -0600

    prompt to cleanup packages

---
+++ install/cleanup.sh
@@ -0,0 +1,7 @@
+UNRESOLVED_PACKAGES=$(apt-get --dry-run autoremove | grep -Po '^Remv \K[^ ]+')
+if [[ -n "$UNRESOLVED_PACKAGES" ]]; then
+  echo It looks like these packages are no longer needed
+  echo "$UNRESOLVED_PACKAGES"
+
+  gum confirm "Remove packages?" && sudo apt autoremove
+fi
